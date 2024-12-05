9cddc4f7f70ce385b96fe91920c7ba1cdd598ea5
devlina.das <devlina.das@accenture.com>
2024-12-05 19:12:23 +0530

    COMMITING REGISTER AND LOGIN REACT CODE

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # coding-project-template
+
+TO RUN the APP:
+
+1. Run Mongosh on local machine
+command Mongosh
+copy the server url from terminal and paste it in 
+/giftlink-backend/util/import-mongo/.env
+/giftlink-backend/.env
+
+2. Run Node server on local
+switch to giftlink-backend 
+then command : npm start will start the node local server on 3060
+
+3. Run React App on Local
+ensure Mongo db and Node server is up and running on local.
+to run react app switch to giftlink-frontend on terminal and run npm start
+
---
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./LoginPage.css";
+
+function LoginPage() {
+  //insert code here to create useState hook variables for email, password
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, seterrorMsg] = useState("");
+
+  // insert code here to create handleLogin function and include console.log
+  const validateEmail = (param)=>{
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(param);
+    }
+
+  const handleLogin = async () => {
+    console.log("Inside handleLogin");
+    if(email === '' || password === ''){
+        seterrorMsg('All fields are mandatory')
+      }else{
+        seterrorMsg('')
+        if(email){
+           if(validateEmail(email)){
+              seterrorMsg('')
+           }else{
+              seterrorMsg('Enter a valid email for example: sample@sampledomain.com')
+           }
+        }
+      }
+  };
+
+  return (
+    <div className="container mt-8">
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-12">
+          <div className="login-card p-4 border rounded">
+            <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+
+            {/* insert code here to create input elements for the variables email and  password */}
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                id="email"
+                type="text"
+                className="form-control"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="form-control"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            {/* insert code here to create a button that performs the `handleLogin` function on click */}
+            {errorMsg && <p style={{color:'red'}}>{errorMsg}</p>}
+
+            <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
+
+            <p className="mt-4 text-center">
+              New here?{" "}
+              <a href="/app/register" className="text-primary">
+                Register Here
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
